77289c3ca444a033e1f15ab536ef049371e8c67d
godkz1234 <54705862+godkz1234@users.noreply.github.com>
2024-10-17 21:43:40 +0900

    (2024.10.17) 생성자와 소멸자

---
+++ Class3th (Constructor & Destructor)/Source.cpp
@@ -0,0 +1,98 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class GameObject
+{
+private:
+	static int count;
+	const char* name;
+
+public:
+	GameObject()
+	{
+		name = "Cylinder";
+
+
+		cout << "Create GameObject" << endl;
+	}
+
+	void Information() const
+	{
+
+		cout << "name : " << name << endl;
+		cout << "Count의 값 : " << count << endl;
+	}
+
+#pragma region 소멸자
+	// 객체가 소멸될 때 자동으로 실행되는 클래스의
+	// 맴버 함수입니다.
+
+	~GameObject()
+	{
+		count++;
+
+		cout << "Destroy GameObject" << endl;
+
+	}
+
+	// 소멸자는 객체가 메모리에서 해제될 때 단 한 번
+	// 만 호출되며, 소멸자에는 매개 변수를 생성하여
+	// 사용할 수 있습니다.
+#pragma endregion
+
+};
+
+int GameObject::count = 0;
+
+void OnDamage(int& stamina)
+{
+	stamina -= 10;
+}
+
+int main()
+{
+#pragma region 생성자
+	// 클래스의 인스턴스가 생성되는 시점에서 자동으로
+	// 호출되는 특수한 멤버 함수입니다.
+
+	// GameObject gameobject;
+	// 
+	// GameObject * GameObject1 = new GameObject();
+	// GameObject * GameObject2 = new GameObject();
+	// GameObject * GameObject3 = new GameObject();
+	// 
+	// 
+	// delete GameObject1;
+	// delete GameObject2;
+	// delete GameObject3;
+	// 
+	// gameobject.Information();
+
+	// 생성자의 경우 객체가 생성될 때 단 한 번만 호출
+	// 되며, 생성자는 반환형이 존재하지 않기 때문에
+	// 생성자가 호출되기 전에는 객체에 대한 메모리는
+	// 할당되지 않습니다.
+#pragma endregion
+
+#pragma region 참조자
+	// 할당된 하나의 메모리 공간에 다른 이름을 
+	// 붙여 사용하는 키워드 입니다.
+
+	// int health = 100;
+	// 
+	// int& reference = health;
+	// 
+	// reference = 99;
+	// 
+	// OnDamage(health);
+	// 
+	// cout << "health 변수의 값 : " << health << endl;
+	// cout << "reference 변수의 값 : " << reference << endl;
+
+#pragma endregion
+
+
+	return 0;
+}
+
