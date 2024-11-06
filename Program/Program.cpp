fbd8c88542ab2121a34cb72fb095f3a584a01f0a
godkz1234 <ddddeong@naver.com>
2024-11-06 21:46:17 +0900

    (2024.11.06) 컨테이너

--- Program/Program.cpp
+++ Program/Program.cpp
@@ -1,40 +1,60 @@
 ﻿#include <iostream> 
-#include <vector>
-#include<list>
-using namespace std;
+#include <stack>
+#include <queue>
 
+using namespace std;
 
 int main()
 {
-#pragma region Vector 컨테이너
+#pragma region 컨테이너 어댑터
+    // 기존의 컨테이너의 인터페이스를 제한하여 만든 기능이
+    // 제한되거나 변형되어 있는 컨테이너입니다.
 
-    // std::vector<int> vector;
+#pragma region Stack
+
+    // std::stack<int> stack;
     // 
-    // vector.reserve(6);
+    // stack.push(10);
+    // stack.push(20);
+    // stack.push(30);
+    // stack.push(40);
     // 
-    // vector.push_back(10); // [10]
-    // vector.push_back(20); // [10][20]
-    // vector.push_back(30); // [10][20][30]
-    // vector.push_back(40); // [10][20][30][40]
-    // vector.push_back(50); // [10][20][30][40][50]
+    // stack.pop();
     // 
-    // vector.pop_back(); // [10][20][30][40]
-    // vector.push_back(50); // [10][20][30][40][50]
-    // vector.push_back(60); // [10][20][30][40][50][60]
-    // vector.push_back(70); // [10][20][30][40][50][60][70]
+    // cout << "Stack의 top의 값 : " << stack.top() << endl;
+    // cout << "Stack의 size의 값 : " << stack.size() << endl;
+    // cout << "Stack의 empty의 값 : " << stack.empty() << endl;
+
+    // int size= stack.size();
     // 
-    // cout << "Capacity : " << vector.capacity() << endl;
-    // cout << "size : " << vector.size() << endl;
-    // 
-    // for (int i = 0; i < vector.size(); i++)
+    // for (int i = 0; i < ; i++)
     // {
-    //     cout << vector[i] << endl;
+    //     cout << "Stack의 top의 값 : " << stack.top() << endl;
+    //     stack.pop();
+    // }
+    
+    // while (stack.empty() == false)
+    // {
+    //     cout << stack.top() << endl;
+    // 
+    //     stack.pop();
     // }
 
 #pragma endregion
 
-#pragma region List 컨테이너
+#pragma region Queue
+    
+    std::queue<int> queue;
 
+    queue.push(10);
+    queue.push(20);
+    queue.push(30);
+
+    queue.pop();
+
+    cout << queue.() << endl;
+
+#pragma endregion
 
 #pragma endregion
 
